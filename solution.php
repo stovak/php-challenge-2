17b4634b04f714bd43d60a36269f8c79de3ba24b
Tom Stovall <stovak@gmail.com>
2017-09-13 12:03:51 -0700

    Add solution for dates_with_at_least_n_scores

--- solution.php
+++ solution.php
@@ -16,9 +16,24 @@ function parse_request($request, $secret)
    return (($expectedSignature == $receivedSignature) ? json_decode($payload, true) : false );
 }
 
-function dates_with_at_least_n_scores($pdo, $n)
+/**
+ * @param $pdo
+ * @param $n
+ *
+ * @return array
+ */
+function dates_with_at_least_n_scores($pdo, $n) : array
 {
-    // YOUR CODE GOES HERE
+   $toReturn = [];
+   $stmt = $pdo->prepare("select date from scores where score = :n order by date DESC");
+   $stmt->bindParam(":n", $n);
+   $result = $stmt->execute();
+   if ($result) {
+      while($record = $stmt->fetch(PDO::FETCH_ASSOC)) {
+         array_push($toReturn, $record['date']);
+      }
+   }
+   return $toReturn;
 }
 
 function users_with_top_score_on_date($pdo, $date)
